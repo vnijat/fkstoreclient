25090d0a6c577abf081383aeb267edfc98ef003e
Nijat <nicatveliyev@gmail.com>
2022-05-17 12:01:07 +0400

    deleteunuseed

--- src/components/pagePagination/index.tsx
+++ src/components/pagePagination/index.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react';
 import { View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { PressableIcon } from '../pressableIcon';
 
 
 interface IPagePAgination {
@@ -15,9 +14,7 @@ export const PagePagination: FC<IPagePAgination> = () => {
 
     return (
         <View >
-            <PressableIcon onPress={()=>console.log("hahahaha Pressed")} >
                 <Icon name="angle-left" size={22} color={'black'} />
-            </PressableIcon>
         </View>
     );
 

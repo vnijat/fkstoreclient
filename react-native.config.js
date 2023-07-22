3da6ea1dd97b5688db4bf7d6435402351ac0dc31
Nijat <nicatveliyev@gmail.com>
2023-07-22 16:29:01 +0400

    disabled soundfor windows

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dependencies: {
+        "react-native-sound": {
+            platforms: {
+                windows: null
+                // add more platform to disable auto-linking for them too
+            }
+        }
+    }
+};
